088e5f1aff9535533cf85a1caba62b1db03f5782
Arpit Mathur <arpit_mathur@hotmail.com>
2025-03-27 00:56:19 -0700

    Speed up

--- src/Board.py
+++ src/Board.py
@@ -43,12 +43,19 @@ class Board:
         return Board(arr_board)
 
     def __init__(self, board: list[list[int]]) -> None:
+
+        self.row_left = [set(range(1, 10)) for _ in range(9)]
+        self.col_left = [set(range(1, 10)) for _ in range(9)]
+        self.box_left = [[set(range(1, 10)) for _ in range(3)] for _ in range(3)]
+        self.board = [[0 for _ in range(9)] for _ in range(9)]
+
         assert len(board) == 9
-        for row in board:
+        for i, row in enumerate(board):
             assert len(row) == 9
-            for cell in row:
+            for j, cell in enumerate(row):
                 assert cell in list(range(10))
-        self.board = board
+                if cell != 0:
+                    self[i, j] = cell
 
     def __str__(self) -> str:
         split_board = list(BLANK)
@@ -65,23 +72,24 @@ class Board:
         return self.board[key[0]][key[1]]
 
     def __setitem__(self, key: tuple[int, int], value: int) -> None:
+        old_value = self.board[key[0]][key[1]]
+        if old_value == value:
+            return
+        if (
+            value not in self.row_left[key[0]]
+            or value not in self.col_left[key[1]]
+            or value not in self.box_left[key[0] // 3][key[1] // 3]
+        ):
+            if value != 0:
+                raise ValueError(f"Invalid value {value} at {key}")
+        if old_value != 0:
+            self.row_left[key[0]].add(old_value)
+            self.col_left[key[1]].add(old_value)
+            self.box_left[key[0] // 3][key[1] // 3].add(old_value)
+
+        if value != 0:
+            self.row_left[key[0]].remove(value)
+            self.col_left[key[1]].remove(value)
+            self.box_left[key[0] // 3][key[1] // 3].remove(value)
+
         self.board[key[0]][key[1]] = value
-
-    def is_valid(self) -> bool:
-        row_left = [set(range(1, 10)) for _ in range(9)]
-        col_left = [set(range(1, 10)) for _ in range(9)]
-        box_left = [set(range(1, 10)) for _ in range(9)]
-
-        try:
-            for i in range(9):
-                for j in range(9):
-                    if self[i, j] == 0:
-                        continue
-                    else:
-                        row_left[i].remove(self[i, j])
-                        col_left[j].remove(self[i, j])
-                        box_left[(i // 3) * 3 + j // 3].remove(self[i, j])
-        except KeyError:
-            return False
-
-        return True
--- src/Solver.py
+++ src/Solver.py
@@ -33,9 +33,11 @@ class Solver:
         for num in range(1, 10):
             if len(self.solutions) >= max_solutions:
                 break
-            self.board[row, col] = num
-            if self.board.is_valid():
+            try:
+                self.board[row, col] = num
                 self._solve(row + 1, col, max_solutions)
+            except ValueError:
+                continue
 
         self.board[row, col] = 0
 
